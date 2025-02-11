0c5bb79bef83d120d2153bae97de00c22c96f5c8
Mohamed Ahmed <mohamedahna1919@gmail.com>
2025-02-11 22:43:14 +0300

    add comments and general description to code

---
+++ file.py
@@ -0,0 +1,9 @@
+"""
+this is a file that will contain most of the work that I will do.
+I'm a beginner to programming and software making in general
+this project is under MIT license because I want everyone to use it.
+I may create a sister project to my later projects because open source is kinda cool
+"""
+# taking inputs from user or a file
+# turing it into a csv file or text file
+# exporting file
